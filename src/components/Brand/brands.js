d3a427026e97431cc7dfc9d2e75401848899b66d
suika7150 <151140029+suika7150@users.noreply.github.com>
2025-11-26 17:53:17 +0800

    2025/11/26 下班前紀錄點

--- src/components/Brand/brands.js
+++ src/components/Brand/brands.js
@@ -1,38 +1,44 @@
 export const brands = [
-  // {
-  //   id: 1,
-  //   name: 'Amazon',
-  //   slug: 'amazon',
-  //   icon: 'amazon.png',
-  //   url: 'https://www.amazon.com/',
-  // },
-
-  // {
-  //   id: 2,
-  //   name: 'Google',
-  //   slug: 'google',
-  //   icon: 'google.png',
-  //   url: 'https://www.google.com/',
-  // },
   {
-    id: 3,
+    id: 1,
     name: 'Apple',
     slug: 'apple',
     icon: 'apple.png',
     url: 'https://www.apple.com/',
   },
   {
-    id: 4,
+    id: 2,
     name: 'Adidas',
     slug: 'adidas',
     icon: 'adidas.png',
     url: 'https://www.adidas.com.tw/',
   },
   {
-    id: 5,
+    id: 3,
     name: 'Kawasaki',
     slug: 'kawasaki',
     icon: 'kawasaki.png',
     url: 'https://www.kawasaki.com/en-us/',
+  },
+  {
+    id: 4,
+    name: 'CocaCola',
+    slug: 'cocacola',
+    icon: 'cocacola.png',
+    url: 'https://www.coca-cola.com/tw/zh',
+  },
+  {
+    id: 5,
+    name: 'Mcdonalds',
+    slug: 'mcdonalds',
+    icon: 'mcdonalds.png',
+    url: 'https://www.mcdonalds.com/tw/zh-tw.html',
+  },
+  {
+    id: 6,
+    name: 'Starbucks',
+    slug: 'starbucks',
+    icon: 'starbucks.png',
+    url: 'https://www.starbucks.com.tw/home/index.jspx',
   },
 ]
