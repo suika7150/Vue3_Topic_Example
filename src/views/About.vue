4343c0f806adfbc7cc1b1577588f44f859f82ded
suika7150 <151140029+suika7150@users.noreply.github.com>
2026-01-29 15:30:36 +0800

    修改關於我們頁面css

--- src/views/About.vue
+++ src/views/About.vue
@@ -197,14 +197,12 @@ onMounted(() => {
 
 <style scoped>
 .canvas-container {
-  position: absolute;
-  /* position: fixed; */
   top: 0;
   left: 0;
   width: 100%;
-  /* height: 100%; */
+  height: 100%;
   min-height: 100vh;
-  /* background: rgb(0, 0, 0); */
+  background: rgb(0, 0, 0);
 }
 canvas {
   display: block;
