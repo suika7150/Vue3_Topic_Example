69625268bf8c7aca647815012f89e5b36dfd8427
suika7150 <151140029+suika7150@users.noreply.github.com>
2026-03-20 14:27:51 +0800

    新增修改註解

--- src/service/api.js
+++ src/service/api.js
@@ -1,65 +1,84 @@
-import Storage, { TOKEN_KEY } from '@/utils/storageUtil'
+/* 使用 apiService.js 建立 axios 實例 */
 import { API_ROUTES } from './apiRoutes'
 import apiService from './apiService'
-import { create } from 'lodash'
 
 const api = {
-  // 註冊
+  /* ===== 使用者相關 API ===== */
+
+  /** 註冊
+   *@param {object} data - 註冊資料{username, password, email}
+   *@returns {Promise} API 回傳 Promise
+   */
   register: (data) => apiService.post(API_ROUTES.REGISTER, data),
 
-  // 登入
+  /** 登入
+   * @param {object} data - 登入資料{username, password}
+   */
   login: (data) => apiService.post(API_ROUTES.LOGIN, data),
 
-  // 用戶資料
+  /** 取得當前使用者資料 */
   user: () => apiService.get(API_ROUTES.USER),
 
-  // 查詢個人資料
+  /** 查詢用戶資料*/
   findUser: () => apiService.get(API_ROUTES.FINDUSER),
 
-  // 個人資料維護
+  /** 更新使用者資料
+   * @param {object} data - 更新個人資料
+   */
   updateProfile: (data) => apiService.put(API_ROUTES.PROFILE, data),
 
-  // 登出
+  /** 登出 */
   logout: () => apiService.post(API_ROUTES.LOGOUT),
 
-  // 新增產品
+  /* ===== 商品相關 API ===== */
+
+  /** 新增商品
+   *@param {object} data - 新增商品資料
+   */
   addProduct: (data) => apiService.post(API_ROUTES.PRODUCT_ADD, data),
 
-  // 取得產品列表 & 搜尋產品
+  /** 取得產品列表 & 搜尋產品*/
   getProducts: (params) => apiService.get(API_ROUTES.PRODUCT_LIST, { params }),
 
-  // 取得商品詳細資料(前台展示)
+  /** 取得商品詳細資料(前台展示)*/
   getProductInfo: (id) => apiService.get(API_ROUTES.PRODUCT_GET(id)),
 
-  // 取得商品詳細資料(後台編輯)
+  /** 取得商品詳細資料(後台管理)*/
   getProductById: (id) => apiService.get(API_ROUTES.PRODUCT_DETAIL(id)),
 
-  // 更新產品資料
+  /**更新商品資料
+    @param {number|string} id - 商品id
+    @param {object} data - 更新商品資料
+    */
   updateProduct: (id, data) => apiService.put(API_ROUTES.PRODUCT_UPDATE(id), data),
 
-  // 取得產品管理列表
+  /** 取得商品管理列表*/
   getManageProducts: () => apiService.get(API_ROUTES.PRODUCT_MANAGE_LIST),
 
-  // 刪除產品
+  /** 刪除商品*/
   deleteProduct: (id) => apiService.put(API_ROUTES.PRODUCT_DELETE(id)),
 
-  // 取得選項列表
+  /* ===== 選項相關 API ===== */
+
+  /** 取得選項列表*/
   getOptions: () => apiService.get(API_ROUTES.OPTIONS_LIST),
 
-  // 新增選項
+  /** 新增選項*/
   addOption: (data) => apiService.post(API_ROUTES.OPTIONS_ADD, data),
 
-  // 更新選項
+  /** 更新選項*/
   updateOption: (id, data) => apiService.put(API_ROUTES.OPTIONS_UPDATE(id), data),
 
-  // 刪除選項
+  /** 刪除選項*/
   deleteOption: (id) => apiService.delete(API_ROUTES.OPTIONS_DELETE(id)),
 
-  // 取得選項列表
+  /** 取得選項列表*/
   getOptionsByListName: (listName) =>
     apiService.get(API_ROUTES.OPTIONS_BY_LISTNAME(), { params: { listName: listName } }),
 
-  //新增訂單
+  /* ===== 訂單相關 API ===== */
+
+  /** 新增訂單*/
   createOrder: (data) => apiService.post(API_ROUTES.ORDER_CREATE, data),
 }
 
