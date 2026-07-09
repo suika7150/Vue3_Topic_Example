6b13cb9415fc0622707b46f4675d00c461601d72
suika7150 <weft71500@gmail.com>
2026-07-09 14:21:13 +0800

    chore:更換雲端空間

--- src/views/crossover/CrossoverView.vue
+++ src/views/crossover/CrossoverView.vue
@@ -12,25 +12,25 @@ const configMap = {
     tag: 'ANIME SERIES',
     title: '動漫聯名',
     subtitle: '二次元的靈魂',
-    video: 'https://silver-paprenjak-f47f04.netlify.app/Videos/Frieren.mp4',
+    video: 'https://res.cloudinary.com/qtzuxouj/video/upload/v1783577303/Frieren_cum7gs.mp4',
   },
   movie: {
     tag: 'MOVIE COLLECTION',
     title: '電影大聯盟',
     subtitle: '經典角色重現',
-    video: 'https://silver-paprenjak-f47f04.netlify.app/Videos/Lion.mp4',
+    video: 'https://res.cloudinary.com/qtzuxouj/video/upload/v1783577524/Lion_ilkeqe.mp4',
   },
   designer: {
     tag: 'DESIGNER SERIES',
     title: '設計師聯名',
     subtitle: '風格的極致',
-    video: 'https://silver-paprenjak-f47f04.netlify.app/Videos/Mushroom.mp4',
+    video: 'https://res.cloudinary.com/qtzuxouj/video/upload/v1783577592/Mechanical_lcuxis.mp4',
   },
   limited: {
     tag: 'LIMITED EDITION',
     title: '全球限量版',
     subtitle: '稀有收藏',
-    video: 'https://silver-paprenjak-f47f04.netlify.app/Videos/Mechanical.mp4',
+    video: 'https://res.cloudinary.com/qtzuxouj/video/upload/v1783577853/Mushroom_nwccpo.mp4',
   },
 }
 
